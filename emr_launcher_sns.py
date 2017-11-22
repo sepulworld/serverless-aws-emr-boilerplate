1826e52dbb04e6986310eaee491e247dd1db9eee
zane <zane.williamson@gmail.com>
2017-11-22 14:19:04 -0800

    Updating to python wordcount

--- emr_launcher_sns.py
+++ emr_launcher_sns.py
@@ -22,31 +22,42 @@ jar_class = os.environ.get('jar_class')
 spark_executor_cores = os.environ.get('spark_executor_cores')
 spark_executor_memory = os.environ.get('spark_executor_memory')
 
-def create_emr_step(additional_args):
-    """Generate a step, takes an argurment list
+def create_emr_python_wordcount_step(additional_args):
+    """Generate a python test step
+aws sns publish \
+  --topic-arn \
+  "arn:aws:sns:us-west-2:12345678910:emr-step-launcher-dev" \
+  --message \
+  "s3://yous3bucket/dev/moby_dick.txt,s3://yours3bucket/dev/moby_dick_output/"
     """
     step = {
-        'Name': "silvermull-powercut-{}".format(env),
-        'ActionOnFailure': 'TERMINATE_JOB_FLOW',
-        'HadoopJarStep': {
-            'Jar': 'command-runner.jar',
-            'Args': [
-                'spark-submit',
-                '--deploy-mode',
-                'cluster',
-                '--class',
-                jar_class,
-                '--conf',
-                'spark.default.parallelism=1000',
-                '--executor-memory',
+        "Name": "python-wordcount-{}".format(env),
+        "ActionOnFailure": "TERMINATE_JOB_FLOW",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "cluster",
+                "--master",
+                "yarn",
+                "--conf",
+                "spark.yarn.submit.waitAppCompletion=false",
+                "--num-executors",
+                "5",
+                "--executor-memory",
                 spark_executor_memory,
-                '--executor-cores',
+                "--executor-cores",
                 spark_executor_cores,
-                jarLocation,
-                additional_args
+                "https://gist.github.com/sepulworld/51fbd6757d3571df0ecb19cc0c1c7403"
             ]
         }
     }
+
+    for i in additional_args:
+        step['HadoopJarStep']['Args'].append(i)
+        logger.info('Generated step: {}'.format(step))
+
     return step
 
 def emr_launcher(event, context):
@@ -61,12 +72,14 @@ ie "s3://silvermullet-data-bucket/input/,s3://silvermullet-data-bucket/output/"
     client = boto3.client('emr')
     sns_event_message = event['Records'][0]['Sns']['Message']
     additional_args = [x for x in sns_event_message.split(',')]
+    logger.info("Complete SNS event: {}".format(event))
     logger.info("Sns message received: {}".format(sns_event_message))
     logger.info("Parsed message into args: {}".format(additional_args))
 
     cluster = client.run_job_flow(
-        Name=emr_name
-        ReleaseLabel=release_label
+        Name=emr_name,
+        ReleaseLabel=release_label,
+        LogUri=emr_loguri,
         ServiceRole='EMR_DefaultRole',
         JobFlowRole='EMR_EC2_DefaultRole',
         VisibleToAllUsers=True,
@@ -85,7 +98,7 @@ ie "s3://silvermullet-data-bucket/input/,s3://silvermullet-data-bucket/output/"
                     ],
                     'LaunchSpecifications': {
                         'SpotSpecification': {
-                            'TimeoutDurationMintues': 30,
+                            'TimeoutDurationMinutes': 30,
                             'TimeoutAction': 'SWITCH_TO_ON_DEMAND'
                         }
                     }
@@ -108,7 +121,7 @@ ie "s3://silvermullet-data-bucket/input/,s3://silvermullet-data-bucket/output/"
                     ],
                     'LaunchSpecifications': {
                         'SpotSpecification': {
-                            'TimeoutDurationMintues': 30,
+                            'TimeoutDurationMinutes': 30,
                             'TimeoutAction': 'SWITCH_TO_ON_DEMAND'
                         }
                     }
@@ -116,7 +129,7 @@ ie "s3://silvermullet-data-bucket/input/,s3://silvermullet-data-bucket/output/"
             ],
             'Ec2KeyName': key_name,
             },
-            Steps=[create_emr_step(additional_args)]
-        )
+        Steps=[create_emr_python_wordcount_step(additional_args)]
+    )
 
-logger.info('EMR cluster launched: {}'.format(cluster))
+    logger.info('EMR cluster launched: {}'.format(cluster))
